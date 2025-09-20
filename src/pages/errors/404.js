5ea69019c0df39506cf57b9478e066cb4a35c679
OR3 SOURCE LEAD <liviesor3hq@gmail.com>
2025-09-20 10:17:19 -0400

    TEST

--- src/pages/errors/404.js
+++ src/pages/errors/404.js
@@ -1,11 +1,13 @@
-// src/pages/404.jsx or wherever your routes live
 import React from 'react';
+import '../../assets/styles/errors/404.css';
 
 const NotFoundPage = () => {
     return (
-        <div style={{ textAlign: 'center', padding: '2rem', paddingTop: '8rem', backgroundColor: '#ffffff' }}>
-            <h1>404 - Page Not Found</h1>
-            <p>Oops! The page you're looking for doesn't exist.</p>
+        <div className="not-found-page">
+            <div className="xyz">
+                <h1>404 - Page Not Found</h1>
+                <p>Oops! The page you're looking for doesn't exist.</p>
+            </div>
         </div>
     );
 };
