63ab2e61698f27b293451ae22407b33e023b8447
OR3 SOURCE LEAD <liviesor3hq@gmail.com>
2025-10-09 11:42:14 -0400

    Update FormPage.js

--- src/pages/forms/FormPage.js
+++ src/pages/forms/FormPage.js
@@ -772,18 +772,15 @@ const FormPage = () => {
                     value={formData.details || ""}
                     readOnly
                 />
-  {[0, 1, 2, 3].map((i) => (
-    <input
-      key={i}
-      type="file"
-      name={`file${i}`}
-      ref={(el) => {
-        if (el) {
-          el.files = files.accepted[i] ? [files.accepted[i]] : new FileList(); // assign actual file
-        }
-      }}
-    />
-  ))}
+                {[0, 1, 2, 3].map((i) => (
+                    <input
+                        key={i}
+                        type="hidden"
+                        name={`file${i}`}
+                        value={files.accepted[i] ? files.accepted[i].name : ""}
+                        readOnly
+                    />
+                ))}
 
             </form>
         </div>
